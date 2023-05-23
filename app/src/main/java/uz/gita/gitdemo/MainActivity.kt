2de5bd240f2b81bca12cb8cccf83c49c6ed4c081
Bexzod <mamatkhalilov@gmail.com>
2023-05-23 13:15:28 +0500

    log

--- app/src/main/java/uz/gita/gitdemo/MainActivity.kt
+++ app/src/main/java/uz/gita/gitdemo/MainActivity.kt
@@ -2,6 +2,7 @@ package uz.gita.gitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,6 @@ class MainActivity : AppCompatActivity() {
 
     fun makeToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        Log.d("TTT",message)
     }
 }
