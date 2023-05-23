99096e1e538402faa7762bad2e535d840854b068
Adam's PC <umidjon.khasimov@gmail.com>
2023-05-23 13:12:32 +0500

    update

--- app/src/main/java/uz/gita/gitdemo/MainActivity.kt
+++ app/src/main/java/uz/gita/gitdemo/MainActivity.kt
@@ -2,10 +2,15 @@ package uz.gita.gitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun makeToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
